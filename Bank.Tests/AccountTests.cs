2d8ef285d3613d837760cf9951c6db8e96b051b0
kitoyaka <vlasiukdima007@gmail.com>
2026-01-09 11:31:20 +0200

    Added 3 unit tests for Account.cs

---
+++ Bank.Tests/AccountTests.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+using Bank.Core.Entities;
+
+namespace Bank.Tests;
+
+public class AccountTests
+{
+    [Fact]
+    public void Debit_WithValidAmount_ShouldReduceBalance()
+    {
+        var clientAccount = new Account { Balance = 1000 }; 
+
+        clientAccount.Debit(200);
+
+        Assert.Equal(800, clientAccount.Balance);
+    }
+
+    [Fact]
+    public void Debit_InsufficientFunds_ShouldThrowException()
+    {
+        var clientAccount = new Account { Balance = 100 };
+
+        var exception = Assert.Throws<Exception>(() => clientAccount.Debit(500));
+
+        Assert.Equal("Insufficient funds.", exception.Message);
+    }
+
+    [Fact]
+    public void Credit_WithValidAmount_ShouldIncreaseBalance()
+    {
+        var clientAccount = new Account { Balance = 100 };
+
+        clientAccount.Credit(300);
+
+        Assert.Equal(400, clientAccount.Balance);
+    }
+}
